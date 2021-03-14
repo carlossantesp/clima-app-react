d1a324d2eb8273ff05eb8b43b1497365ca9f39ba
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-13 23:30:01 -0400

    Agregando el componente Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const Formulario = () => {
+  const [search, setSearch] = useState({
+    ciudad: "",
+    pais: "",
+  });
+  const [error, setError] = useState(false);
+
+  const { ciudad, pais } = search;
+
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      ) : null}
+      <div className="input-field col s12">
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
+        <label htmlFor="ciudad">Ciudad:</label>
+      </div>
+
+      <div className="input-field col s12">
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
+          <option value="">-- Selecione un país--</option>
+          <option value="BO">Bolivia</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="pais">País: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          value="Buscar Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
