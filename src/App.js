7a4b328beab00a8d852ff972d4946d10df1940ff
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-14 01:05:09 -0400

    Validando datos para la API incorrecto 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from "react";
 import Clima from "./components/Clima";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
+import Error from "./components/Error";
 
 function App() {
   const [search, setSearch] = useState({
@@ -10,6 +11,7 @@ function App() {
   });
   const [consultar, setConsultar] = useState(false);
   const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
 
   const { ciudad, pais } = search;
 
@@ -23,11 +25,25 @@ function App() {
         const resultado = await resp.json();
 
         setResultado(resultado);
-        setConsultar(true);
+        setConsultar(false);
+
+        //Detecta si hubo resultados correctos en la consulta
+        if (resultado.cod === "404") {
+          setError(true);
+        } else {
+          setError(false);
+        }
       }
     };
     consultaAPI();
   }, [consultar]);
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No hat resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
 
   return (
     <Fragment>
@@ -42,9 +58,7 @@ function App() {
                 setConsultar={setConsultar}
               />
             </div>
-            <div className="col m6 s12">
-              <Clima resultado={resultado} />
-            </div>
+            <div className="col m6 s12">{componente}</div>
           </div>
         </div>
       </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Error from "./Error";
 
 const Formulario = ({ search, setSearch, setConsultar }) => {
   const [error, setError] = useState(false);
@@ -25,9 +26,7 @@ const Formulario = ({ search, setSearch, setConsultar }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      {error ? (
-        <p className="red darken-4 error">Todos los campos son obligatorios</p>
-      ) : null}
+      {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
       <div className="input-field col s12">
         <input
           type="text"
