bf42231067ae56e3a05dfd0d4fa5d1fd56bde8e8
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-16 20:00:16 -0400

    Agregando estilos tailwindcss al componente Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
+import tw, { styled } from "twin.macro";
+
+const FooterContainer = styled.footer`
+  ${tw`flex justify-center items-center p-1 bg-gray-900`}
+  p {
+    ${tw`text-gray-300 text-center text-xs`}
+  }
+  a {
+    ${tw`no-underline text-gray-200 font-semibold`}
+  }
+`;
 
 const Footer = ({ autor }) => {
   return (
-    <footer>
-      <p>{autor}</p>
-    </footer>
+    <FooterContainer>
+      <p>
+        Diseñado y Desarrollado por{" "}
+        <a href="https://carlos.santillan.dev" target="blank">
+          {autor}
+        </a>
+      </p>
+    </FooterContainer>
   );
+};
+
+Footer.propTypes = {
+  autor: PropTypes.string.isRequired,
 };
 
 export default Footer;
